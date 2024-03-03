ea86577bbb1b7539d75d592d0e45a808b9d95d63
sketchworthy <tinytianyi@gmail.com>
2024-03-03 11:43:56 -0800

    Updated setNoteLocations to work for 1 music note but method needs fixing all around

--- src/Level1Scene.js
+++ src/Level1Scene.js
@@ -202,6 +202,11 @@ class Level1Scene extends Phaser.Scene {
 	 * randomize music note locations
 	 */
 	setNoteLocations() { // TODO make this for multiple music notes
+		var x = Phaser.Math.Between(-100, 100);
+		var y = Phaser.Math.Between(-50, 0);
+		this.musicNote.setPosition(x,y);
+		this.musicNote.setScale(0);
+
 		// // these are the possible positions the coin can move to
 		// let positions = [
 		// 	{ x: 120, y: 135 }, { x: 680, y: 135 },
@@ -213,8 +218,8 @@ class Level1Scene extends Phaser.Scene {
 		// positions = positions.filter((p) => !(p.x === this.coin.x && p.y === this.coin.y));
 
 		// let newPosition = Phaser.Math.RND.pick(positions);
-		// this.coin.setPosition(newPosition.x, newPosition.y);
-		// this.coin.setScale(0);
+		// this.musicNote.setPosition(newPosition.x, newPosition.y);
+		// this.musicNote.setScale(0);
 
 		// this.tweens.add({
 		// 	targets: this.coin,
